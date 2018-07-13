ecabe53a83725e1b9e7c358c20161920c7c659d7
vkrivcov <krivcov.vadim@gmail.com>
2018-07-13 21:57:38 +0100

    enabling to specify customised topic partitions and replication factor

--- scripts/create-topic.sh
+++ scripts/create-topic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]; then
-    $KAFKA_HOME/bin/kafka-topics.sh --zookeeper vkc-zk1:2181 --replication-factor 3 --partitions 1 --topic $1 --create
+if [ $# -gt 2 ]; then
+    $KAFKA_HOME/bin/kafka-topics.sh --zookeeper vkc-zk1:2181 --topic $1 --create --replication-factor $2 --partitions $3 
 else
-    echo "Usage: "$(basename $0)" <topic_name>"
+    echo "Usage: "$(basename $0)" <topic_name> <replication-factor> <partitions>"
 fi
 
